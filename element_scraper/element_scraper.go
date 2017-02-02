0dc9cafb8744d006f2a03f6f46db28725b284852
Matti Ahinko <matti.ahinko@gofore.com>
2017-02-02 23:08:07 +0200

    Add constructor with response and add function for getting element children

--- element_scraper/element_scraper.go
+++ element_scraper/element_scraper.go
@@ -1,23 +1,52 @@
-package element_scraper
+package elementscraper
 
 import (
-  "github.com/PuerkitoBio/goquery"
-  "io"
+	"io"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 type ElementScraper struct {
-  Document *goquery.Document
+	Document *goquery.Document
+}
+
+func NewElementScraperFromResponse(resp *http.Response) (ElementScraper, error) {
+	doc, err := goquery.NewDocumentFromResponse(resp)
+	if err != nil {
+		return ElementScraper{nil}, err
+	}
+
+	return ElementScraper{doc}, nil
 }
 
 func NewElementScraperFromReader(reader io.Reader) (ElementScraper, error) {
-  doc, err := goquery.NewDocumentFromReader(reader)
-  if err != nil {
-    return ElementScraper{nil}, err
-  }
+	doc, err := goquery.NewDocumentFromReader(reader)
+	if err != nil {
+		return ElementScraper{nil}, err
+	}
 
-  return ElementScraper{doc}, nil
+	return ElementScraper{doc}, nil
 }
 
-func (elementScraper *ElementScraper) Find(element string) (string)  {
-  return elementScraper.Document.Find(element).Contents().Text()
+func (elementScraper *ElementScraper) Find(element string) string {
+	return elementScraper.Document.Find(element).Contents().Text()
+}
+
+func (elementScraper *ElementScraper) FindInside(element string, children []string) []map[string]string {
+	childrenElements := make([]map[string]string, 0)
+	elementScraper.Document.Find(element).Each(func(i int, s *goquery.Selection) {
+		row := make(map[string]string)
+		found := false
+		for _, child := range children {
+			row[child] = s.Find(child).Text()
+			if len(row[child]) > 0 {
+				found = true
+			}
+		}
+		if found {
+			childrenElements = append(childrenElements, row)
+		}
+	})
+	return childrenElements
 }
